bf8961b9f53f15b32ab263b172d7f13464e8f45f
Justin Rosenthal <justin.rosenthal@gmail.com>
2021-11-23 14:18:46 -0800

    Add k8s context to prompt

--- config.fish
+++ config.fish
@@ -15,35 +15,21 @@ set -g theme_newline_prompt "\$ "
 functions --copy fish_prompt fish_prompt_original
 function fish_prompt; echo; fish_prompt_original; end
 
-# bobthefish theme
-set -g theme_color_scheme dracula
+# Enable k8s context segment
+set -g theme_display_k8s_context yes
 
-# My color scheme
-set -U fish_color_normal normal
-set -U fish_color_command F8F8F2
-set -U fish_color_quote F1FA8C
-set -U fish_color_redirection 8BE9FD
-set -U fish_color_end 50FA7B
-set -U fish_color_error FF5555
-set -U fish_color_param 5FFFFF
-set -U fish_color_comment 6272A4
-set -U fish_color_match --background=brblue
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
-set -U fish_color_operator 00a6b2
-set -U fish_color_escape 00a6b2
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion BD93F9
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_cancel -r
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D yellow
-set -U fish_pager_color_prefix white --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
+# bobthefish theme
+function bobthefish_colors -S -d 'Define a custom bobthefish color scheme'
+    # Inherit from the dracula theme
+    __bobthefish_colors dracula
+
+    # These were copied from __bobthefish_colors.fish
+    set -l bg       282a36
+    set -l green    50fa7b
+
+    # Better legibility for the k8s context segment
+    set -x color_k8s $green $bg --bold
+end
 
 # Override the nix prompt for the theme so that we show a more concise prompt
 function __bobthefish_prompt_nix -S -d 'Display current nix environment'

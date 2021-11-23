f773a7b602579f3b9b3a24da18af6f9c84d36462
Justin Rosenthal <justin.rosenthal@gmail.com>
2021-11-23 14:15:56 -0800

    Prevent tmux from using a login shell

--- home.nix
+++ home.nix
@@ -172,6 +172,7 @@
     ];
 
     extraConfig = ''
+      set -g default-command fish
       set -g default-terminal "xterm-256color"
       set-option -ga terminal-overrides ",xterm-256color:Tc"
       set -g mouse on

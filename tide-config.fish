84b356dc7609aa56e79f1e4fba9a5a31d04dcd60
Justin Rosenthal <justin.rosenthal@gmail.com>
2026-06-02 16:59:34 -0700

    Fix tide prompt config file

--- tide-config.fish
+++ tide-config.fish
@@ -1,8 +1,159 @@
-set -U _tide_left_items pwd set -U _tide_left_items git set -U _tide_left_items newline set -U _tide_left_items character
-set -U _tide_prompt_12254 \e\[38\;2\;52\;101\;164\;49m\e\[48\;2\;52\;101\;164m\ @PWD@\ \e\[38\;2\;52\;101\;164\;48\;2\;78\;154\;6m\e\[48\;2\;78\;154\;6m\ \e\[37m\e\[38\;2\;0\;0\;0m05-28-add_redshift_anal…\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \*3\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \e\[38\;2\;78\;154\;6\;49m set -U _tide_prompt_12254 \e\[38\;2\;95\;215\;0m❮ set -U _tide_prompt_12254 \e\[38\;2\;215\;175\;0\;49m\e\[38\;2\;0\;0\;0\;48\;2\;215\;175\;0m\ ▼\ \e\[38\;2\;68\;136\;62\;48\;2\;215\;175\;0m\e\[38\;2\;0\;0\;0\;48\;2\;68\;136\;62m\ \ 22.22.0\ \e\[38\;2\;247\;76\;0\;48\;2\;68\;136\;62m\e\[38\;2\;0\;0\;0\;48\;2\;247\;76\;0m\ \ 1.93.1\ \e\[38\;2\;255\;153\;0\;48\;2\;247\;76\;0m\e\[38\;2\;35\;47\;62\;48\;2\;255\;153\;0m\ \ staging/us-east-1\ \e\[38\;2\;211\;215\;207\;48\;2\;255\;153\;0m\e\[38\;2\;0\;0\;0\;48\;2\;211\;215\;207m\ 12:49:36\ PM\ \e\[38\;2\;211\;215\;207\;49m
-set -U _tide_prompt_12384 \e\[38\;2\;52\;101\;164\;49m\e\[48\;2\;52\;101\;164m\ @PWD@\ \e\[38\;2\;52\;101\;164\;48\;2\;196\;160\;0m\e\[48\;2\;196\;160\;0m\ \e\[37m\e\[38\;2\;0\;0\;0mmain\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ !2\e\[38\;2\;0\;0\;0m\ \e\[38\;2\;196\;160\;0\;49m set -U _tide_prompt_12384 \e\[38\;2\;95\;215\;0m❮ set -U _tide_prompt_12384 \e\[38\;2\;126\;186\;228\;49m\e\[38\;2\;0\;0\;0\;48\;2\;126\;186\;228m\ \ impure\ \e\[38\;2\;211\;215\;207\;48\;2\;126\;186\;228m\e\[38\;2\;0\;0\;0\;48\;2\;211\;215\;207m\ 04:55:20\ PM\ \e\[38\;2\;211\;215\;207\;49m
-set -U _tide_prompt_4642 \e\[38\;2\;52\;101\;164\;49m\e\[48\;2\;52\;101\;164m\ @PWD@\ \e\[38\;2\;52\;101\;164\;48\;2\;78\;154\;6m\e\[48\;2\;78\;154\;6m\ \e\[37m\e\[38\;2\;0\;0\;0mmain\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \*3\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \e\[38\;2\;78\;154\;6\;49m set -U _tide_prompt_4642 \e\[38\;2\;95\;215\;0m❮ set -U _tide_prompt_4642 \e\[38\;2\;215\;175\;0\;49m\e\[38\;2\;0\;0\;0\;48\;2\;215\;175\;0m\ ▼\ \e\[38\;2\;68\;136\;62\;48\;2\;215\;175\;0m\e\[38\;2\;0\;0\;0\;48\;2\;68\;136\;62m\ \ 22.22.0\ \e\[38\;2\;247\;76\;0\;48\;2\;68\;136\;62m\e\[38\;2\;0\;0\;0\;48\;2\;247\;76\;0m\ \ 1.93.1\ \e\[38\;2\;255\;153\;0\;48\;2\;247\;76\;0m\e\[38\;2\;35\;47\;62\;48\;2\;255\;153\;0m\ \ staging/us-east-1\ \e\[38\;2\;211\;215\;207\;48\;2\;255\;153\;0m\e\[38\;2\;0\;0\;0\;48\;2\;211\;215\;207m\ 04:48:10\ PM\ \e\[38\;2\;211\;215\;207\;49m
-set -U _tide_prompt_4646 \e\[38\;2\;52\;101\;164\;49m\e\[48\;2\;52\;101\;164m\ @PWD@\ \e\[38\;2\;52\;101\;164\;48\;2\;78\;154\;6m\e\[48\;2\;78\;154\;6m\ \e\[37m\e\[38\;2\;0\;0\;0mmain\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \*1\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \e\[38\;2\;78\;154\;6\;49m set -U _tide_prompt_4646 \e\[38\;2\;95\;215\;0m❮ set -U _tide_prompt_4646 \e\[38\;2\;215\;175\;0\;49m\e\[38\;2\;0\;0\;0\;48\;2\;215\;175\;0m\ ▼\ \e\[38\;2\;68\;136\;62\;48\;2\;215\;175\;0m\e\[38\;2\;0\;0\;0\;48\;2\;68\;136\;62m\ \ 22.22.0\ \e\[38\;2\;247\;76\;0\;48\;2\;68\;136\;62m\e\[38\;2\;0\;0\;0\;48\;2\;247\;76\;0m\ \ 1.93.1\ \e\[38\;2\;255\;153\;0\;48\;2\;247\;76\;0m\e\[38\;2\;35\;47\;62\;48\;2\;255\;153\;0m\ \ staging/us-east-1\ \e\[38\;2\;211\;215\;207\;48\;2\;255\;153\;0m\e\[38\;2\;0\;0\;0\;48\;2\;211\;215\;207m\ 12:06:38\ PM\ \e\[38\;2\;211\;215\;207\;49m
-set -U _tide_prompt_4648 \e\[38\;2\;52\;101\;164\;49m\e\[48\;2\;52\;101\;164m\ @PWD@\ \e\[38\;2\;52\;101\;164\;48\;2\;78\;154\;6m\e\[48\;2\;78\;154\;6m\ \e\[37m\e\[38\;2\;0\;0\;0m06-02-add_east1_analyti…\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \*3\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \e\[38\;2\;78\;154\;6\;49m set -U _tide_prompt_4648 \e\[38\;2\;95\;215\;0m❮ set -U _tide_prompt_4648 \e\[38\;2\;215\;175\;0\;49m\e\[38\;2\;0\;0\;0\;48\;2\;215\;175\;0m\ ▼\ \e\[38\;2\;68\;136\;62\;48\;2\;215\;175\;0m\e\[38\;2\;0\;0\;0\;48\;2\;68\;136\;62m\ \ 22.22.0\ \e\[38\;2\;247\;76\;0\;48\;2\;68\;136\;62m\e\[38\;2\;0\;0\;0\;48\;2\;247\;76\;0m\ \ 1.93.1\ \e\[38\;2\;255\;153\;0\;48\;2\;247\;76\;0m\e\[38\;2\;35\;47\;62\;48\;2\;255\;153\;0m\ \ tooling/us-east-1\ \e\[38\;2\;211\;215\;207\;48\;2\;255\;153\;0m\e\[38\;2\;0\;0\;0\;48\;2\;211\;215\;207m\ 04:52:24\ PM\ \e\[38\;2\;211\;215\;207\;49m
-set -U _tide_prompt_4664 \e\[38\;2\;52\;101\;164\;49m\e\[48\;2\;52\;101\;164m\ @PWD@\ \e\[38\;2\;52\;101\;164\;48\;2\;78\;154\;6m\e\[48\;2\;78\;154\;6m\ \e\[37m\e\[38\;2\;0\;0\;0mmain\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \*1\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\e\[38\;2\;0\;0\;0m\ \e\[38\;2\;78\;154\;6\;49m set -U _tide_prompt_4664 \e\[38\;2\;95\;215\;0m❮ set -U _tide_prompt_4664 \e\[38\;2\;215\;175\;0\;49m\e\[38\;2\;0\;0\;0\;48\;2\;215\;175\;0m\ ▼\ \e\[38\;2\;68\;136\;62\;48\;2\;215\;175\;0m\e\[38\;2\;0\;0\;0\;48\;2\;68\;136\;62m\ \ 22.22.0\ \e\[38\;2\;247\;76\;0\;48\;2\;68\;136\;62m\e\[38\;2\;0\;0\;0\;48\;2\;247\;76\;0m\ \ 1.93.1\ \e\[38\;2\;255\;153\;0\;48\;2\;247\;76\;0m\e\[38\;2\;35\;47\;62\;48\;2\;255\;153\;0m\ \ staging/us-east-1\ \e\[38\;2\;211\;215\;207\;48\;2\;255\;153\;0m\e\[38\;2\;0\;0\;0\;48\;2\;211\;215\;207m\ 10:32:21\ AM\ \e\[38\;2\;211\;215\;207\;49m
-set -U _tide_right_items status set -U _tide_right_items cmd_duration set -U _tide_right_items context set -U _tide_right_items jobs set -U _tide_right_items direnv set -U _tide_right_items node set -U _tide_right_items python set -U _tide_right_items rustc set -U _tide_right_items java set -U _tide_right_items ruby set -U _tide_right_items go set -U _tide_right_items kubectl set -U _tide_right_items terraform set -U _tide_right_items aws set -U _tide_right_items nix_shell set -U _tide_right_items time
+set -g tide_aws_bg_color FF9900
+set -g tide_aws_color 232F3E
+set -g tide_aws_icon 
+set -g tide_bun_bg_color FBF0DF
+set -g tide_bun_color 14151A
+set -g tide_bun_icon 󰳓
+set -g tide_character_color 5FD700
+set -g tide_character_color_failure FF0000
+set -g tide_character_icon ❯
+set -g tide_character_vi_icon_default ❮
+set -g tide_character_vi_icon_replace ▶
+set -g tide_character_vi_icon_visual V
+set -g tide_cmd_duration_bg_color C4A000
+set -g tide_cmd_duration_color 000000
+set -g tide_cmd_duration_decimals 0
+set -g tide_cmd_duration_icon
+set -g tide_cmd_duration_threshold 3000
+set -g tide_context_always_display false
+set -g tide_context_bg_color 444444
+set -g tide_context_color_default D7AF87
+set -g tide_context_color_root D7AF00
+set -g tide_context_color_ssh D7AF87
+set -g tide_context_hostname_parts 1
+set -g tide_crystal_bg_color FFFFFF
+set -g tide_crystal_color 000000
+set -g tide_crystal_icon 
+set -g tide_direnv_bg_color D7AF00
+set -g tide_direnv_bg_color_denied FF0000
+set -g tide_direnv_color 000000
+set -g tide_direnv_color_denied 000000
+set -g tide_direnv_icon ▼
+set -g tide_distrobox_bg_color FF00FF
+set -g tide_distrobox_color 000000
+set -g tide_distrobox_icon 󰆧
+set -g tide_docker_bg_color 2496ED
+set -g tide_docker_color 000000
+set -g tide_docker_default_contexts default colima
+set -g tide_docker_icon 
+set -g tide_elixir_bg_color 4E2A8E
+set -g tide_elixir_color 000000
+set -g tide_elixir_icon 
+set -g tide_gcloud_bg_color 4285F4
+set -g tide_gcloud_color 000000
+set -g tide_gcloud_icon 󰊭
+set -g tide_git_bg_color 4E9A06
+set -g tide_git_bg_color_unstable C4A000
+set -g tide_git_bg_color_urgent CC0000
+set -g tide_git_color_branch 000000
+set -g tide_git_color_conflicted 000000
+set -g tide_git_color_dirty 000000
+set -g tide_git_color_operation 000000
+set -g tide_git_color_staged 000000
+set -g tide_git_color_stash 000000
+set -g tide_git_color_untracked 000000
+set -g tide_git_color_upstream 000000
+set -g tide_git_icon
+set -g tide_git_truncation_length 24
+set -g tide_git_truncation_strategy
+set -g tide_go_bg_color 00ACD7
+set -g tide_go_color 000000
+set -g tide_go_icon 
+set -g tide_java_bg_color ED8B00
+set -g tide_java_color 000000
+set -g tide_java_icon 
+set -g tide_jobs_bg_color 444444
+set -g tide_jobs_color 4E9A06
+set -g tide_jobs_icon 
+set -g tide_jobs_number_threshold 1000
+set -g tide_kubectl_bg_color 326CE5
+set -g tide_kubectl_color 000000
+set -g tide_kubectl_icon 󱃾
+set -g tide_left_prompt_frame_enabled false
+set -g tide_left_prompt_items pwd git newline character
+set -g tide_left_prompt_prefix ''
+set -g tide_left_prompt_separator_diff_color 
+set -g tide_left_prompt_separator_same_color 
+set -g tide_left_prompt_suffix 
+set -g tide_nix_shell_bg_color 7EBAE4
+set -g tide_nix_shell_color 000000
+set -g tide_nix_shell_icon 
+set -g tide_node_bg_color 44883E
+set -g tide_node_color 000000
+set -g tide_node_icon 
+set -g tide_os_bg_color 333333
+set -g tide_os_color D6D6D6
+set -g tide_os_icon 
+set -g tide_php_bg_color 617CBE
+set -g tide_php_color 000000
+set -g tide_php_icon 
+set -g tide_private_mode_bg_color F1F3F4
+set -g tide_private_mode_color 000000
+set -g tide_private_mode_icon 󰗹
+set -g tide_prompt_add_newline_before true
+set -g tide_prompt_color_frame_and_connection 6C6C6C
+set -g tide_prompt_color_separator_same_color 949494
+set -g tide_prompt_icon_connection ' '
+set -g tide_prompt_min_cols 34
+set -g tide_prompt_pad_items true
+set -g tide_prompt_transient_enabled false
+set -g tide_pulumi_bg_color F7BF2A
+set -g tide_pulumi_color 000000
+set -g tide_pulumi_icon 
+set -g tide_pwd_bg_color 3465A4
+set -g tide_pwd_color_anchors E4E4E4
+set -g tide_pwd_color_dirs E4E4E4
+set -g tide_pwd_color_truncated_dirs BCBCBC
+set -g tide_pwd_icon
+set -g tide_pwd_icon_home
+set -g tide_pwd_icon_unwritable 
+set -g tide_pwd_markers .bzr .citc .git .hg .node-version .python-version .ruby-version .shorten_folder_marker .svn .terraform bun.lockb Cargo.toml composer.json CVS go.mod package.json build.zig
+set -g tide_python_bg_color 444444
+set -g tide_python_color 00AFAF
+set -g tide_python_icon 󰌠
+set -g tide_right_prompt_frame_enabled false
+set -g tide_right_prompt_items status cmd_duration context jobs direnv bun node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig time
+set -g tide_right_prompt_prefix 
+set -g tide_right_prompt_separator_diff_color 
+set -g tide_right_prompt_separator_same_color 
+set -g tide_right_prompt_suffix ''
+set -g tide_ruby_bg_color B31209
+set -g tide_ruby_color 000000
+set -g tide_ruby_icon 
+set -g tide_rustc_bg_color F74C00
+set -g tide_rustc_color 000000
+set -g tide_rustc_icon 
+set -g tide_shlvl_bg_color 808000
+set -g tide_shlvl_color 000000
+set -g tide_shlvl_icon 
+set -g tide_shlvl_threshold 1
+set -g tide_status_bg_color 2E3436
+set -g tide_status_bg_color_failure CC0000
+set -g tide_status_color 4E9A06
+set -g tide_status_color_failure FFFF00
+set -g tide_status_icon ✔
+set -g tide_status_icon_failure ✘
+set -g tide_terraform_bg_color 800080
+set -g tide_terraform_color 000000
+set -g tide_terraform_icon 󱁢
+set -g tide_time_bg_color D3D7CF
+set -g tide_time_color 000000
+set -g tide_time_format '%r'
+set -g tide_toolbox_bg_color 613583
+set -g tide_toolbox_color 000000
+set -g tide_toolbox_icon 
+set -g tide_vi_mode_bg_color_default 949494
+set -g tide_vi_mode_bg_color_insert 87AFAF
+set -g tide_vi_mode_bg_color_replace 87AF87
+set -g tide_vi_mode_bg_color_visual FF8700
+set -g tide_vi_mode_color_default 000000
+set -g tide_vi_mode_color_insert 000000
+set -g tide_vi_mode_color_replace 000000
+set -g tide_vi_mode_color_visual 000000
+set -g tide_vi_mode_icon_default D
+set -g tide_vi_mode_icon_insert I
+set -g tide_vi_mode_icon_replace R
+set -g tide_vi_mode_icon_visual V
+set -g tide_zig_bg_color F7A41D
+set -g tide_zig_color 000000
+set -g tide_zig_icon 
